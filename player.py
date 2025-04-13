f2d9bfe7d7ff03fdee27711589173196bcabd8b2
Jerry Leong <junfai2007@gmail.com>
2025-04-13 15:12:51 +0800

    deleted player file

--- player.py
+++
@@ -1,7 +0,0 @@
-import grid as grid
-
-class Player:
-    def tileSelection(self, tile):
-        userSelection = []
-        for i in range (1, len(grid.correctOrder)):
-            print("Select tile number", i)
